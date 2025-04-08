7681897ecec099393445556142cd2dd5ad15106f
Ayesha Deshwali <139254189+AyeshaDeshwali@users.noreply.github.com>
2025-04-08 17:43:06 +0530

    Update api.js

--- WeatherApp/weather-frontend/src/api.js
+++ WeatherApp/weather-frontend/src/api.js
@@ -1,4 +1,4 @@
-const BASE_URL = "http://localhost:5000/api";
+const BASE_URL = process.env.REACT_APP_API_URL;
 
 export const getWeatherByCoords = async (lat, lon) => {
   const response = await fetch(
